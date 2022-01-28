12d149a24e3f5a12400429b9f1569907b7f7b0e3
Alex Malitsky <1457274+amalitsky@users.noreply.github.com>
2022-01-27 20:14:19 -0800

    feat(hw1): Add meetupTitle radio

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,71 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const template = `
+  <div>
+    <label v-for="n of 5" :key="n">
+      <input
+        type="radio"
+        :value="n"
+        v-model="meetupId"
+        :disabled="loading"
+      /> {{ n }}
+    </label>
+    <hr />
+
+    <h3 v-show="!loading">{{ meetupHeader }}</h3>
+    <h4 v-show="loading">loading...</h4>
+  </div>
+`;
+
+const app = createApp({});
+
+const Meetups = defineComponent({
+  data() {
+    return {
+      meetupId: 1,
+      loading: false,
+      meetupData: null,
+    };
+  },
+  methods: {
+    async loadMeetup(id) {
+      // we need to introduce `requestId` here to avoid
+      // race condition and respect the most recent call
+      // seems like an overkill for now
+      if (this.loading) {
+       return Promise.reject('Currently loading');
+      }
+
+      this.loading = true;
+      this.meetupData = null;
+
+      const meetupData = await fetchMeetupById(id);
+
+      this.loading = false;
+      this.meetupData = meetupData;
+    },
+  },
+  computed: {
+    meetupHeader() {
+      return this.meetupData?.title || '';
+    },
+  },
+  created() {
+    this.loadMeetup(this.meetupId);
+  },
+  watch: {
+    meetupId(newId, oldId) {
+      if (newId === oldId) {
+        return;
+      }
+
+      this.loadMeetup(newId);
+    },
+  },
+  template,
+});
+
+app.component('meetups', Meetups);
+
+app.mount('#app');
