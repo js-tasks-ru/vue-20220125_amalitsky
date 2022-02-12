5f75a935eb1ff2300a8368f4c6b50916b19e921b
Alex Malitsky <1457274+amalitsky@users.noreply.github.com>
2022-02-12 00:30:46 -0800

    feat(hw6): Add UiInput component

--- 06-wrappers/03-UiInput/components/UiInput.vue
+++ 06-wrappers/03-UiInput/components/UiInput.vue
@@ -1,13 +1,37 @@
 <template>
-  <div class="input-group input-group_icon input-group_icon-left input-group_icon-right">
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+  <div
+    class="input-group"
+    :class="{
+      'input-group_icon': $slots['left-icon'] || $slots['right-icon'],
+      'input-group_icon-left': $slots['left-icon'],
+      'input-group_icon-right': $slots['right-icon'],
+    }"
+  >
+    <div
+      v-if="$slots['left-icon']"
+      class="input-group__icon"
+    >
+      <slot name="left-icon"/>
     </div>
 
-    <input ref="input" class="form-control form-control_rounded form-control_sm" />
+    <component
+      :is="tag"
+      ref="input"
+      v-bind="$attrs"
+      :value="modelValueProxy"
+      :class="{
+        'form-control': true,
+        'form-control_rounded': rounded,
+        'form-control_sm': small,
+      }"
+      @input="modelValueProxy = $event.target.value"
+    ></component>
 
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+    <div
+      v-if="$slots['right-icon']"
+      class="input-group__icon"
+    >
+      <slot name="right-icon"/>
     </div>
   </div>
 </template>
@@ -15,6 +39,37 @@
 <script>
 export default {
   name: 'UiInput',
+
+  inheritAttrs: false,
+
+  props: {
+    small: Boolean,
+    rounded: Boolean,
+    multiline: Boolean,
+    modelValue: [String, Number],
+  },
+
+  emits: ['update:modelValue'],
+
+  computed: {
+    tag() {
+      return this.multiline ? 'textarea' : 'input';
+    },
+    modelValueProxy: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      },
+    },
+  },
+
+  methods: {
+    focus() {
+      this.$refs.input.focus();
+    },
+  },
 };
 </script>
 
