fa6ce392a28e99d1493e28927e8a852caa24f379
Alex Malitsky <1457274+amalitsky@users.noreply.github.com>
2022-02-18 21:59:15 -0800

    feat(hw7): Add CounterButton component

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,46 @@
 <template>
-  <button></button>
+  <button @click="increment()">{{ localCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  data() {
+    return {
+      localCount: this.count,
+    };
+  },
+
+  watch: {
+    count(newCountValue) {
+      this.localCount = newCountValue;
+    },
+  },
+
+  methods: {
+    increment() {
+      this.localCount++;
+
+      this.$emit('update:count', this.localCount);
+    },
+  },
 };
 </script>
+
+<style scoped>
+
+button {
+  padding: 0.5rem 1rem;
+}
+
+</style>
